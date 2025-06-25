35f8195c3ac6ff85d207ee8475b278d89c3f5056
Asia Amodeo <asiaamodeo@gmail.com>
2025-06-25 12:49:51 +0100

    mapbox token

---
+++ js/config.js
@@ -0,0 +1,6 @@
+
+const CONFIG = {
+    mapboxToken: 'pk.eyJ1IjoiYXNpYWFtb2RlbyIsImEiOiJjbGFiMHE4eWUwY3piM3BwZWd5ajRoajBuIn0.Bu2rSIQlVd5_gVI13VprSA', 
+    mapCenter: [-0.1276, 51.5074], 
+    mapZoom: 11
+}; 
